38a5018fcecfaad8c7f6735e803a197286c76311
郭杰 <guojie@synpowertech.com>
2020-02-19 16:17:43 +0800

    1.change

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-let demo = (param) => {
-    console.log('hello ' + param)
+export function setLocalStorage(name, data) {
+    localStorage.setItem(name, data)
+}
+export function setSessionStorage(name, data) {
+    sessionStorage.setItem(name, data)
 }
 
-export {
-    demo
+
+export function getLocalStorage(name) {
+    return localStorage.getItem(name)
+}
+export function getSessionStorage(name) {
+    return sessionStorage.getItem(name)
+}
+
+export function clearLocalStorage() {
+    return localStorage.clear()
+}
+export function clearSessionStorage() {
+    return sessionStorage.clear();
+}
+
+export function removeSessionStorage(name) {
+    return sessionStorage.removeItem(name)
 }
